278ba831c8441b102d74a35bdc372e5fcef60b93
calebj4 <calebjohnson@Calebs-Laptop.local>
2023-04-03 23:34:44 -0400

    index show edit and delete routes now functional

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 // Dependencies
 const express = require('express');
 const mongoose = require("mongoose")
-const Product = require('./models/products');
+const product = require('./models/products');
+const methodOverride = require("method-override")
 const app = express();
 require('dotenv').config();
 
-mongoose.connect(process.env.DATABASE_URL);
 // Database Connection Error/Success
 // Define callback functions for various events
 const db = mongoose.connection
@@ -14,72 +14,92 @@ db.on('connected', () => console.log('mongo connected'));
 db.on('disconnected', () => console.log('mongo disconnected'));
 
 
+
 // Middleware
 // Body parser middleware: give us access to req.body
 app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
+
+mongoose.connect(process.env.DATABASE_URL);
 
 
 
-app.get('/products/new', (req, res) => {
-    res.render('new.ejs')
-})
-// INDUCES
 
 // I is for Index
-
-// Index
 app.get('/products', async (req, res) => {
-    const allProducts = await Product.find({})
+    const allProducts = await product.find({})
     res.render('index.ejs', {
-        Product: allProducts
+        product: allProducts
     }
     );
 }); 
 
+
 // N is for NEW
 app.get('/products/new', (req, res) => {
-	res.send('new');
+	res.render('new.ejs');
 });
+
+
+
 
 // D is for DELETE
 app.delete('/products/:id', async(req, res) => {
-    await Product.findbByIdAndDelete(req.params.id);
-    res.redirect('/product');
+    await product.findByIdAndDelete(req.params.id);
+    res.redirect('/products');
 });
+
+
+// S is for SHOW
+app.get('/products/:id', async(req, res) => {
+    const selectedProduct = await product.findById(req.params.id).exec()
+        res.render('show.ejs',
+    {
+        product: selectedProduct,
+    })
+    })
+
 
 // U is for UPDATE
 app.put('/product/:id', async (req, res) => {
-    await Product.findByAndUpdate(
+    await product.findByAndUpdate(
         req.params.id,
         req.body,
         { new: true }
     );
-    res.redirect('/product');
+    res.redirect('/products/${req.params.id}');
 });
 
+
+
+
 // C is for CREATE
-app.post('/products', (req,res) => {
-    if (req.body.completed === 'on') {
-        //if checked, req.body.completed is set to 'on'
-        req.body.completed = true;
-    } else {
-        //if not checked, req.body.completed is undefined
-        req.body.completed = false;
-    }
-    const createdProduct = new Product(req.body)
+app.post('/products', (req, res) => {
+    const createdProduct = new product(req.body)
     createdProduct.save().then(res.redirect('/products'))
 })
 
+
+
+
 // E is for EDIT
-app.get ('product/:id/edit', async (req,res) => {
-    const foundProduct= await Product.findById (reg.params.id).exec();
-    res.render('edit.ejs', {Product: seedProduct });
+app.get ('/products/:id/edit', async (req, res) => {
+    const selectedProduct= await product.findById (req.params.id).exec();
+    res.render('edit.ejs', {product: selectedProduct });
 })
-// S is for SHOW
-app.get('/products/:id', async(req, res) => {
-    let i = req.params.id
-    let foundProducts = await Product.find({})
-    res.render("show.ejs", {product: foundProducts[i]});
+
+
+
+// // S is for SHOW
+// app.get ('/products/:id'), async(req, res) => {
+// const selectedProduct = await Product.findById(req.params.id).exec()
+//     res.render('show.ejs',
+// {
+//     product: selectedProduct,
+// })
+// }
+
+
 
 // Listener
 const PORT = process.env.PORT;
